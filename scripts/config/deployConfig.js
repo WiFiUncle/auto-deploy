935212048d493002291741f1f6f7417494b1a89a
WiFi_Uncle <wifi_uncle@163.com>
2021-10-26 17:13:10 +0800

    doc:  文档修改

--- scripts/config/deployConfig.js
+++ scripts/config/deployConfig.js
@@ -2,16 +2,11 @@
 export default {
   dev: {
     scpOpt: {
-      host: '175.6.0.226',
-      port: '33322',
-      username: 'wuxian',
-      password: 'wifi$%147',
-      path: '/home/wuxian/auto-deploy'
-      // host: '', // 服务器主机名
-      // port: '', // 端口
-      // username: '', // 用户名
-      // password: '', // 密码
-      // path: '' // 部署路径
+      host: '', // 服务器主机名
+      port: '', // 端口
+      username: '', // 用户名
+      password: '', // 密码
+      path: '' // 部署路径
     }
   },
   test: {
